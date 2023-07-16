0a1ad9f9c2333f3ab007ef7546273fbad2819769
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-16 22:28:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function fibonacci(num) {
-   let a = 1;
+   let a = 0;
    let b = 1;
    if(num<=2) return a;
 	let c;
-	for(let i=3;i<num;i++){
+	for(let i=3;i<=num;i++){
 		    c = a+b;
 		    a= b;
 		    b = c;	    
